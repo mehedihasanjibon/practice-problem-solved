482ad9b739d1fec922a15ae166fa95cf65220d79
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-04 02:09:16 +0600

    problem2 solved

--- problem2.js
+++ problem2.js
@@ -6,7 +6,26 @@ The first line of the input is the marks of the five subjects mentioned above, r
 
 Output:
 print the result in 2 decimal places.  
+
+Sample input:
+    75.25, 65, 80, 35.45, 99.50
+
+    output: 
+        71.04
 */
 
+var mathMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+var totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+console.log(totalMarks);
+
+var average = totalMarks / 5;
+// console.log(average);
+var averageTwoDecimal = average.toFixed(2);
+console.log(averageTwoDecimal);
 
 
