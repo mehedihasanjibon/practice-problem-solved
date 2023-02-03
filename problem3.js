d01eb88a5f889049cd7693324924940c4273e804
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-04 02:19:48 +0600

    problem3 question complete

---
+++ problem3.js
@@ -0,0 +1,16 @@
+/* 
+Sarah's mother is teaching her mathematics. She gave Sarah a number and asked her what the remainder would be if she divided the number by 5. Help Sarah write the program.
+
+Input: 
+The first line of the input contains the Number
+
+Output:
+print the remainder. 
+
+sample Input: 
+    119
+
+    Output: 
+        4 
+*/
+
