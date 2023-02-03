d05128ffde84c844a587333af08f1da3e0610243
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-04 02:26:57 +0600

    problem4 question complete

---
+++ problem4.js
@@ -0,0 +1,7 @@
+/* 
+You are given an array:
+        var fruits = ["apple", "Banana" , "Orange"];
+
+(a) Find the index of "Banana" and replace "Banana" with "Mango".
+(b) Remove "Orange" and add "Watermelon". 
+*/
