636f772047f6ae53593cdf90ab166a06c500b28e
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-04 02:31:48 +0600

    problem4 solved

--- problem4.js
+++ problem4.js
@@ -5,3 +5,12 @@ You are given an array:
 (a) Find the index of "Banana" and replace "Banana" with "Mango".
 (b) Remove "Orange" and add "Watermelon". 
 */
+
+var fruits = ["Apple" , "Banana" , "Orange"];
+var bananaIndex = fruits.indexOf("Banana");
+fruits[bananaIndex] = "Mango";
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push("Watermelon");
+console.log(fruits);
