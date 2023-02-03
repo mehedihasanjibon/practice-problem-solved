18d32409965fe67fbfbd2282ed04f7c1b8e431b0
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-04 02:01:13 +0600

    problem2 question complete

---
+++ problem2.js
@@ -0,0 +1,12 @@
+/* 
+write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student. 
+
+Input:
+The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+Output:
+print the result in 2 decimal places.  
+*/
+
+
+
